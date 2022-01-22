2b9639a9336f8839efc583c6f3c5c711693665a6
Bedran Yildirim <bedran_1999@hotmail.com>
2022-01-22 23:10:31 +0100

    added dog command

---
+++ commands/info/dog.js
@@ -0,0 +1,14 @@
+const { default: axios } = require('axios') 
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "dog",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async function({client, message, args})  {
+        axios.get("https://dog.ceo/api/breeds/image/random").then(function (res) {
+            const dog = res.data;
+            message.channel.send(dog.message)
+    })
+}}
