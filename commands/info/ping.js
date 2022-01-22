612830a12234df5f6b323732a1db1df5163f9d45
Bedran Yildirim <bedran_1999@hotmail.com>
2022-01-22 23:00:35 +0100

    Weather command added

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,9 +1,8 @@
 module.exports = {
-  name: "ping",
-  category: "info",
-  permissions: [],
-  devOnly: false,
-  run: async({client, message, args}) => {
-    message.reply("Pong");
+  name: 'ping',
+  permissions:[],
+  run: async ({bot, message, client}) => {
+      const msg = await message.channel.send('Pinging...')
+      msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`)
   }
 }
