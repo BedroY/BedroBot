9ff6a1de7fcca252d11175c1abd7987d429c866f
Bedran Yildirim <bedran_1999@hotmail.com>
2022-02-08 20:49:26 +0100

    lyrics command

---
+++ commands/info/lyrics.js
@@ -0,0 +1,37 @@
+const ftl = require("findthelyrics");
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = {
+  name: "lyrics",
+  category: "music",
+  permissions: [],
+  devOnly: false,
+  run: async ({ client, message, args }) => {
+    if (args.length === 0) {
+      message.channel.send("Please enter a film!");
+    } else {
+      ftl.find(args, function (err, resp) {
+        if (!err) {
+          console.log(resp);
+          message.channel.send("No result! Please try again!");
+        } else {
+          console.log(err);
+        }
+
+        const exampleEmbed = new MessageEmbed()
+          .setTitle(`${capitalizeFirstLetter(args)} 🎱`)
+          .setDescription(resp)
+          .setTimestamp()
+          .setFooter({
+            text: "BedroBot",
+            iconURL:
+              "https://cdn.discordapp.com/attachments/634648075809325075/935627937993076776/Allmight.png",
+          });
+
+        message.channel.send({ embeds: [exampleEmbed] });
+      });
+    }
+  },
+};
